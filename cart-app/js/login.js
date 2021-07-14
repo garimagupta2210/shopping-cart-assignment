caf832939eb86f09943a364fecfa377348547ebb
Garima <gar1@publicisgroupe.net>
2021-07-14 12:17:29 +0530

    initial commit

---
+++ cart-app/js/login.js
@@ -0,0 +1,23 @@
+var testEmail = "test@gmail.com";
+var testPassword = "test@1234"
+
+
+function validateUser(event){
+    event.preventDefault();
+    var validationCheck = true;
+    var em= $('#email').val()
+     var pwd= $('#password').val()
+     document.getElementById('email-alert').style.display = "none";
+     document.getElementById('password-alert').style.display = "none";
+     if(em != testEmail){
+         document.getElementById('email-alert').style.display = "block";
+         validationCheck = false;
+     }
+     if(pwd != testPassword){
+        document.getElementById('password-alert').style.display = "block";
+        validationCheck = false;
+    }
+    if(validationCheck == true){
+        location.href = "home.html";
+    }
+}
